f61cbed3ccbb1d10e134beab576d00530ec4ff10
wispelaere <ibizaa62@gmail.com>
2024-02-06 15:59:38 +0100

    version1.1

--- js exercices algo darmangeat/asset/scripts/script.js
+++ js exercices algo darmangeat/asset/scripts/script.js
@@ -102,7 +102,6 @@
 // let heure = parseInt(prompt("donnez une heure"));
 // let minute = parseInt(prompt("donnez les minutes")) +1;
 
-
 // if (minute == 60) {
 //   minute = 0;
 //   heure = heure + 1;
@@ -115,7 +114,6 @@
 
 // exercice 4.3
 
-
 // let heure = parseInt(prompt("donnez une heure"));
 // let minute = parseInt(prompt("donnez les minutes")) ;
 // let seconde = parseInt(prompt("donnez les secondes")) ;
@@ -126,7 +124,6 @@
 //     seconde = 0;
 //     minute = minute + 1;
 // }
-
 
 // if (minute == 60){
 //   minute = 0;
@@ -164,7 +161,6 @@
 // if(sexe  && age > 20){
 //     alert("il paie des impots");
 
-
 // }
 // else if (!sexe  && (age > 18 && age < 35)){
 // alert("elle paie des impots et des passe droit");
@@ -173,4 +169,160 @@
 // else{
 //    alert("tu ne paie pas d'impot mais oublie pas de verser les sous a papa");
 // }
+
+// exercice 4.7
+
+// let old = prompt("quelle âges-avez vous ?");
+// let permit = prompt("Depuis combien de temps avez vous votre permis ?");
+// let responsibleAccident = prompt("Avez-vous deja eu un ou plusieurs accident résponsables?");
+// let fidelity = prompt("Depuis combien de temp êtes-vous chez nous ?")
+// let count = 0;
+// let blues = 4;
+// let green = 3;
+// let orange = 2;
+// let red = 1;
+
+// if ((responsibleAccident >= 3) || (old < 25 && permit < 2 && responsibleAccident == 0)){
+//     alert("vous êtes refusé");
+// }
+
+// else {
+
+// if (responsibleAccident < 1){
+//     count += 1;
+// }
+
+// if (old > 25){
+//     count += 1;
+// }
+
+// if (permit > 2){
+//     count += 1;
+// }
+
+// if(fidelity > 1){
+//     count += 1;
+// }
+
+// if(count == 1){
+// alert("vous êtes assuré couleur red");
+// }
+// if(count == 2){
+// alert("vous êtes assuré couleur orange");
+// }
+// if(count == 3){
+// alert("vous êtes assuré couleur vert");
+// }
+// if(count == 4){
+//  alert("vous êtes assuré couleur bleues");
+//  }
+
+// }
+
+// exercice 5.1
+
+// let i = prompt("Donnez un nombres entre 1 et 3 !");
+
+// while(i < 1 || i > 3){
+// i = prompt("Donnez un nombre entre 1 et 3");
+
+// }
+// alert("vous avez choissis " + i);
+
+// exercice 5.2
+
+// let i = prompt("Donnez un nombres entre 10 et 20 !");
+
+// while (i < 10 || i > 20) {
+//   if (i < 10) {
+//     alert("Plus grand!");
+//     i = prompt("Donnez un nombre entre 10 et 20");
+//   }
+
+//   if (i > 20) {
+//     alert("plus petit!");
+//     i = prompt("Donnez un nombre entre 10 et 20");
+//   }
+// }
+
+// exercice 5.3
+
+// let i = parseInt(prompt("Donnez un nombres!"));
+// /* je creer une variable j pour faire le calcul avec le i*/
+
+// for(let j=i+1; j<=i+10 ; j++){
+//     alert(j)
+// }
+
+// exercice 5.4
+
+// let number = (prompt("Donnez un nombres!"));
+
+// document.write("<h3> table de " + number+ "<h3>");
+
+// for(let i = 1 ; i<=10 ; i++){
+
+//     document.write(number + "x" + i + "=" + number * i+ 5
+//      "<br>");
+// }
+
+// exercice 5.5
+
+// let number = parseInt(prompt("Donnez un nombres!"));
+// let sum = 0;
+
+// for(let i = 1   ; i <= number; i++ ){
+//     sum = sum + i;
+//     console.log(sum);
+// }
+// alert(sum);
+
+// exercice 5.6
+
+// let number = parseInt(prompt("Donnez un nombres!"));
+// let multiplication = 1;
+
+// for(let i = 1   ; i <= number; i++ ){
+//     multiplication= parseInt(multiplication * i);
+//     console.log(multiplication);
+// }
+// alert(multiplication);
+
+// exercice 5.7
+// let number;
+// let numberMax = 0;
+// let position = 0;
+
+// for (let i = 1; i < 11; i++) {
+//   number = parseInt(prompt("Veuillez saisir un nombre svp !"));
+//   if (number > numberMax) {
+//     numberMax = number;
+//     position = i;
+
+//   }
+// }
+// alert(numberMax);
+// alert(position)
+
+// exercice 5.8
+
+// let number = 1;
+// let numberMax = 0;
+// let position = 0;
+// let place = 0;
+// while (number != 0) {
+//   number = parseInt(prompt("Veuillez saisir un nombre svp !"));
+
+//   position = position + 1;
+//   if (number > numberMax) {
+//     numberMax = number;
+//     place = position;
+//   }
+// }
+// alert(numberMax);
+// alert(place);
+
+exercice 5.9
+
+
 

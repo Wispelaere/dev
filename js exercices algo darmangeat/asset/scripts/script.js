bada51f9565bfb2362234e1688a9620119b8c3a5
wispelaere <ibizaa62@gmail.com>
2024-02-07 16:34:27 +0100

    version 1.2

--- js exercices algo darmangeat/asset/scripts/script.js
+++ js exercices algo darmangeat/asset/scripts/script.js
@@ -322,7 +322,212 @@
 // alert(numberMax);
 // alert(place);
 
-exercice 5.9
+// exercice 5.9
+
+// let price = 1;
+// let sum=0;
+// let returned =0;
+// let paperMoney_05 = 0;
+// let paperMoney_10 = 0;
+// let coin = 0;
+
+// while(price != 0){
+//     price = parseInt(prompt("Veuillez saisir les prix des achats !"));
+//     sum = price + sum;
+// }
+// alert("montant total a payer est de " + sum);
+
+
+// let amountPaid= parseInt(prompt("entrez le montant payé par le client"));
+
+// let change= amountPaid - sum;
+// alert(change);
+
+
+// if(amountPaid==sum){
+//     alert("merci au revoir");
+// }
+//  else{
+
+// while (change >= 10) { // Tant que la monnaie est supérieur à 10
+//   paperMoney_10 = paperMoney_10 + 1; // Je retire 10 et j'ajoute +1 a ma valeur billetde10
+//   change = change - 10;
+// }
+
+// while (change >= 5) {
+//   change = change - 5;// Je retire 5 et j'ajoute +1 à ma valeur billetde5
+//   paperMoney_05 = paperMoney_05 + 1;
+// }
+
+// while (change >= 1) {
+//   change = change - 1; // Je retire 1 et j'ajoute 1 a ma valeur piecedeun
+//   coin = coin +1;
+// }
+
+// alert(
+//   "il va rendre " +
+//     paperMoney_10 +
+//     " en billet de 10 " +
+//     paperMoney_05 +
+//     " en billet de 5 " +
+//     coin +
+//     " en pieces"
+// );
+// }
 
 
 
+// exercice 6.1
+
+// let myArray = Array(15); //je lui donne une taille de 15
+
+// for(let i = 0; i<myArray.length;i++){ // ici je lui dit tu commence a i=0 donc premiere place dans le tableau et tu arrete a 15 qui est la taille de mon tableau
+//     myArray[i]=0; // ici tu lui dit que ta variable i = 0 dans le tableau met tu aurait pus mettre par exemple u nnombre donnez par une variable nombre
+// }
+// console.log(myArray);
+
+// exerice 6.3
+
+// let myArray = Array(9);
+
+// for(let i = 0; i<myArray.length;i++){
+//     let number = prompt("donnez des chiffres")
+//     myArray[i]=number;
+// }
+// alert(myArray);
+
+// exercice 6.7
+
+
+// let myArray = Array(9);
+// let sum =0 ;
+
+// for(let i = 0; i<myArray.length;i++){
+//     let number = parseInt(prompt("donnez des chiffres"));
+//     sum = sum + number;
+//     myArray[i]=number;
+    
+// }
+// let average = sum / myArray.length // variable moyenne en anglais // je dvise ma sum de mes mes nombre additionner avant par la longueur du tableau avec un .lenght
+// alert(myArray);
+// alert("la moyenne est de : " + average)
+
+// exercice 6.8
+
+// let myArray = [];
+// let positiv =0;
+// let negativ = 0;
+
+// let numberValue = parseInt(prompt("Entre le nombres de valeur pour le tableau"));            // je lui demande combien de valeur dans le tableau
+
+// for(i=0 ; i<numberValue; i++){
+//   let value =  parseInt(prompt("donnez une valeurs")); 
+//     myArray.push(value);                                     // ici je pousse les valeurs dans le tableau 
+
+// console.log(myArray);
+
+// if(value<0){
+//     negativ = negativ+1;              // j'ai mis un compteur de valeur +1 a chaque valu négative en dessou de 0 par exemple
+// }
+// if(value>0){
+//     positiv = positiv+1;
+// }}
+// console.log("il y aura en négatives : " + negativ);
+// console.log("il y aura en positives : " + positiv);
+
+// exercice 6.9
+
+// let sum =0 ;
+
+
+// let numberValue = parseInt(prompt("Combien de valeurs ?")); 
+// let myArray =Array(numberValue);      //en gros , le tableau prendra la taille que j'ai deefini par le nombres de valeurs rentré
+// for(let i = 0; i<numberValue;i++){
+//  myArray[i]=parseInt(prompt("Donnez un nombre")); // donc la tableau et ma valeur [i] , elle est defini a chaque boucle
+
+//    sum += myArray[i]; //chaque tour , ta somme prend la valeur dans le tableau
+// }
+// console.log("la somme des nombres dans le tableau est " + sum);
+
+// exercice 6.10
+
+// let myArray1 = [4,8,7,9,1,5,4,6];
+// let myArray2 = [7,6,5,2,1,3,7,4];
+// let myArray3 = [];
+
+// for(i=0;i<myArray1.length;i++){
+//   let sum=  myArray1[i] + myArray2[i];    // je lui dit fait l'adition des valeur dans myArray1 et myArray2 et met sa dans une valeur sim 
+// myArray3.push(sum);   // ensuite tu pousse la valeur sum donc les chiffre adittioné des 2 tableau dans le tableau 3
+// }
+// console.log("le tableau sera  " +  myArray3 );
+
+// exercice 6.11
+
+// let myArray1 = [4, 8, 7, 12];       // solution 1 difficile
+// let myArray2 = [3, 6];
+// let sum = 0;
+
+// for (let i = 0; i < myArray1.length; i++) {
+//     sum += myArray2[0] * myArray1[i];
+// }
+
+// for (let i = 0; i < myArray1.length; i++) {
+//     sum += myArray2[1] * myArray1[i];
+// }
+
+// console.log(sum);
+
+
+// let myArray1 = [4, 8, 7, 12];   // je donne un tableau
+// let myArray2 = [3, 6];
+// let sum = 0;
+
+// for (let i = 0; i < myArray1.length; i++) {    // ici je lui dit prend tableau 1
+//     for (let j = 0; j < myArray2.length; j++) {    // ici prend tableau 2
+//     sum += myArray2[j] * myArray1[i];    // je fait une boucle pour mutilpier mes deux premiere valeur dans chaque valeur et je les adddtionne
+// }
+// }
+
+
+
+// console.log(sum);
+
+// exercice 6.12
+
+// let myArray = [];
+
+
+
+// let numberValue = parseInt(prompt("Entre le nombres de valeur pour le tableau"));            // je lui demande combien de valeur dans le tableau
+
+// for(i=0 ; i<numberValue; i++){
+//   let value =  parseInt(prompt("donnez une valeurs")); 
+//     myArray.push(value+1);    
+  
+// }
+
+
+// console.log(myArray);
+
+// exercice 6.12
+
+let myArray = [];
+let position = 0;
+
+
+
+
+let numberValue = parseInt(prompt("Entre le nombres de valeur pour le tableau"));            // je lui demande combien de valeur dans le tableau
+
+for(i=0 ; i<numberValue; i++){
+  let value =  parseInt(prompt("donnez une valeurs")); 
+    myArray.push(value);    
+  
+    if(value>0){
+     position+1;
+    }
+}
+
+
+console.log(myArray);
+console.log(position);
